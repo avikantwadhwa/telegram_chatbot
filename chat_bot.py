173a6a8255fa102e032b22ac97239349940db596
avikant wadhwa <avikant16@hotmail.com>
2019-03-14 22:31:28 +0530

    error_debug

--- chat_bot.py
+++ chat_bot.py
@@ -3,23 +3,26 @@ import requests
 import configparser as cfg
 
 class telegram_chatbot():
+
+    def read_token_from_config_file(self,config):
+            parser=cfg.ConfigParser()
+            parser.read(config)
+            return parser.get('creds','token')
+
     def __init__(self,config):
         self.token=self.read_token_from_config_file(config)
         self.base="https://api.telegram.org/bot{}/".format(self.token)
         
-        def get_updates(self,offset=None):
+    def get_updates(self,offset=None):
             url=self.base + "/getUpdates?&timeout=100"
             if offset:
                 url=url+"&offset={}".format(offset+1)
                 r=requests.get(url)
                 return json.loads(r.content)
         
-        def sendMessage(self,msg,chat_id):
-            url=self.base + "/sendMessage?chat_id={}&text={}".foramt(chat_id,msg)
+    def sendMessage(self,msg,chat_id):
+            url=self.base + "/sendMessage?chat_id={}&text={}".format(chat_id,msg)
             if msg is not None:
                 r=requests.get(url)
 
-        def read_token_from_config_file(config):
-            parser=cfg.ConfigParser()
-            parser.read(config)
-            return parser.get('creds','token')
+     
