b25d0f461daaf97617191ec9db75fbf906b9d824
avikant wadhwa <avikant16@hotmail.com>
2019-03-18 23:43:35 +0530

    json

--- server.py
+++ server.py
@@ -4,12 +4,14 @@ bot=chat_bot.telegram_chatbot("797382536:AAE7xX2VuyfzuCQaQlXlc5-syjwcJF0qU54")
 update_id=None
 
 def make_reply(message):
-    reply="okay"
+    if message is not None:
+        reply="okay"
     return reply
 while True:
-    print("...")
+    #print("...")
     updates=bot.get_updates(offset=update_id)
-    updates=updates["result"]
+    if updates:
+        updates=updates["result"]
     if updates:
         for item in updates:
 
@@ -21,5 +23,5 @@ while True:
             from_=item["message"]["from"]["id"]
 
             reply=make_reply(message)
-
+            
             bot.sendMessage(reply,from_)
